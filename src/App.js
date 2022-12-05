093ae2bfa6a12f23053a9e9dd8ee167f26acff03
Adel <adelk.bargach@gmail.com>
2022-12-05 17:57:57 +0100

    push to github

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import { nanoid } from "nanoid";
+import FilterButton from "./components/FilterButton";
+import Form from "./components/Form";
+import Todo from "./components/todo"
 
-function App() {
+
+
+
+
+function App(props) {
+  
+  const [tasks, setTasks] = useState(props.tasks);
+
+  const taskList = tasks.map((task) => (
+    <Todo 
+      id={task.id} 
+      name={task.name} 
+      key={task.id}
+      deleteTask={deleteTask}
+      editTask={editTask}
+      />
+    ));
+
+    function addTask(name) {
+      const newTask = { id: `todo-${nanoid()}`, name, completed: false };
+      setTasks([...tasks, newTask]);
+    }
+
+    function deleteTask(id) {
+      const remainingTasks = tasks.filter((task) => id !== task.id);
+      setTasks(remainingTasks);
+    }
+
+    function editTask(id, newName) {
+      const editedTaskList = tasks.map((task) =>{
+        if (id === task.id) {
+          return {...task, name: newName}
+        }
+        return task;
+      });
+      setTasks(editedTaskList);
+    }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="todoapp stack-large">
+      <h1>Todo List</h1>
+      <Form addTask={addTask} />
+      
+      <ul
+        className="todo-list stack-large stack-exception"
+        aria-labelledby="list-heading">
+        {taskList}
+      </ul>
     </div>
   );
 }
 
-export default App;
+export default App
